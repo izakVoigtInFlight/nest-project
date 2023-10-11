8bea88f91c4a7843ebd81cb2c65b9ff4a717ca02
Izak Voigt <izak.voigt@inflightdublin.com>
2023-10-11 12:17:56 +0100

    fix webpack config

--- apps/assets-service/webpack.config.js
+++ apps/assets-service/webpack.config.js
@@ -1,12 +1,13 @@
 const { composePlugins, withNx } = require('@nx/webpack');
+const nodeExternals = require('webpack-node-externals');
 
-const swcDefaultConfig =
-  require('@nestjs/cli/lib/compiler/defaults/swc-defaults').swcDefaultsFactory()
-    .swcOptions;
+const swcDefaultConfig = require('@nestjs/cli/lib/compiler/defaults/swc-defaults').swcDefaultsFactory().swcOptions;
 
-module.exports = composePlugins(withNx(), (config) => {
-  // Rule setted to fix class-validator adaptation with webpack
-  // If removed, class-validator will not execute any validation at any point
+module.exports = composePlugins(withNx({ skipTypeChecking: false }), config => {
+  // Custom configurations setted to maximize compatibility between NestJS and NX following some configs from NestJS CLI
+  // See: https://github.com/nestjs/nest-cli/blob/master/lib/compiler/defaults/webpack-defaults.ts
+  config.target = 'node';
+  config.ignoreWarnings = [/^(?!CriticalDependenciesWarning$)/];
   config.module.rules.push({
     test: /\.tsx?$/,
     exclude: /node_modules/,
@@ -14,7 +15,17 @@ module.exports = composePlugins(withNx(), (config) => {
       loader: 'swc-loader',
       options: swcDefaultConfig,
     },
-  })
+  });
+  config.externals = [nodeExternals()];
+  config.externalsPresets = { node: true };
+  config.mode = 'none';
+  config.optimization = {
+    nodeEnv: 'false',
+  };
+  config.node = {
+    __filename: false,
+    __dirname: false,
+  };
 
   return config;
 });
